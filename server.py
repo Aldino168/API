8800057e332d686e30cbbb7006771c8c993ceffe
Ahnaf Zainul Muttaqin <ahnafzainul@gmail.com>
2022-05-28 10:27:44 +0700

    add 2 methods, PUT for update user and DEL for delete user

--- server.py
+++ server.py
@@ -1,18 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template
 import pymongo
 import json
 from bson.objectid import ObjectId
-
-import pandas as pd
-import pickle
-import os
-
-
 import ocr
 from PIL import Image
-import torch
-import easyocr
-import re
+
 
 app=Flask(__name__)
 #######################################
@@ -27,12 +19,16 @@ try:
 except: 
     print("ERROR - Cannot connect to mongodb")
 #######################################
+@app.route('/')
+def home():
+   return render_template('temp.html')
+
 @app.route('/users', methods=['POST'])
 def create_user():
     try:
         csvfile = request.files["file"]
         img = Image.open(csvfile)
-        img = img.save("data/images/zzzz.jpg")
+        img = img.save("data/images/zzzz.jpg") 
         konek = ocr.connect()
         semua = ocr.diskon1(konek)
         user = {
@@ -54,8 +50,14 @@ def create_user():
             mimetype = "application/json"
         )
 
-    except Exception as e:
-        print(e)
+    except:
+        return Response(
+            response= json.dumps(
+                {
+                    "massage": "user cant created"
+                }
+            ), status = 500,
+            mimetype = "application/json")
 #######################################
 @app.route('/users', methods=["GET"])
 def read_user():
@@ -79,7 +81,67 @@ def read_user():
 
 
 #######################################
+@app.route("/users/<id>", methods=["PUT"])
+def update_user(id):
+    try:
+        csvfile = request.files["file"]
+        img = Image.open(csvfile)
+        img = img.save("data/images/zzzz.jpg") 
+        konek = ocr.connect()
+        semua = ocr.diskon1(konek)
+        
+        dbRespose = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {'$push':{"disc" : {'$each' : semua[0]}}})
+        dbRespose = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {'$push':{"hargadisc" : {'$each' : semua[1]}}})
+        dbRespose = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {'$push':{"harga" : {'$each' : semua[2]}}})
+        dbRespose = db.users.update_one(
+            {"_id":ObjectId(id)},
+            {'$push':{"produk" : {'$each' : semua[3]}}})
 
+        return Response(
+            response= json.dumps({"massage": "user updated"}),
+            status = 200,
+            mimetype = "application/json")
+
+    except Exception as e:
+        print(e)
+        return Response(
+            response= json.dumps(
+                {"massage": "cannot update users"})
+                , status = 500,
+                mimetype = "application/json"
+        )
+#######################################
+
+@app.route("/users/<id>", methods=["DELETE"])
+def delete_user(id):
+    try:
+        dbResponse = db.users.delete_one({"_id":ObjectId(id)})
+        if dbResponse.deleted_count == 1:
+            return Response(
+                response= json.dumps({"massage": "user deleted", "id" : f"{id}"}),
+                status = 200,
+                mimetype = "application/json")
+        
+        return Response(
+                response= json.dumps({"massage": "user not found", "id" : f"{id}"}),
+                status = 200,
+                mimetype = "application/json")
+
+    except Exception as e:
+        print(e)
+        return Response(
+            response= json.dumps(
+                {"massage": "cannot update users"})
+                , status = 500,
+                mimetype = "application/json"
+        )
+#######################################
 
 if __name__ == "__main__":
     app.run(port=80, debug=True)
